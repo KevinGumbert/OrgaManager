76ca26407e75a0d057cc1919f34d4be048a6243e
Jochen1980 <info@jochen-bauer.net>
2014-01-25 10:59:33 +0100

    test local commit

--- src/orgamanager/Main.java
+++ src/orgamanager/Main.java
@@ -6,11 +6,11 @@ import orgamanager.view.OmView;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// SwingMVC, see macs.hw.ac.uk/guidebook/?name=Using%20The%20GUI&page=1
+        public static void main(String[] args) {
         OmModel model = new OmModel();
         OmView view = new OmView();
         OmController controller = new OmController(model, view);
-	}
+        controller.prepareForView();
+        }
 
 }
