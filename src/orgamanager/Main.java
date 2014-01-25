de477068df4a65c980ca46b02cd357f9a31b2b4d
Jochen1980 <info@jochen-bauer.net>
2014-01-25 11:05:59 +0100

    formatted

--- src/orgamanager/Main.java
+++ src/orgamanager/Main.java
@@ -6,11 +6,11 @@ import orgamanager.view.OmView;
 
 public class Main {
 
-        public static void main(String[] args) {
-        OmModel model = new OmModel();
-        OmView view = new OmView();
-        OmController controller = new OmController(model, view);
-        controller.prepareForView();
-        }
+	public static void main(String[] args) {
+		OmModel model = new OmModel();
+		OmView view = new OmView();
+		OmController controller = new OmController(model, view);
+		controller.prepareForView();
+	}
 
 }
