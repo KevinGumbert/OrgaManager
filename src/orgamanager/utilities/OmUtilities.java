f066ffc1b1a48a8b8c1768e6f6d5531d04dc5b0e
jay <jay@jay-Aspire-V5-571G>
2014-01-27 00:36:40 +0100

    asdf

--- src/orgamanager/utilities/OmUtilities.java
+++ src/orgamanager/utilities/OmUtilities.java
@@ -1,9 +1,12 @@
 package orgamanager.utilities;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
@@ -51,6 +54,24 @@ public class OmUtilities {
 		// TODO Mac
 	}
 	
+	public boolean createDir(String path){
+		boolean success = (new File(path)).mkdirs();
+		if (success) {
+		    return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean createFile(String path){
+		File file = new File(path);
+		if (file.exists()){
+		    return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public String getProperty(String key){
 		Reader reader = null;
 		String value = "";
@@ -66,6 +87,36 @@ public class OmUtilities {
 		}
 		return value;
 	}
+	
+	public boolean exec(String command, String param){
+		JOptionPane.showMessageDialog(null, ("Exec-Call: " + command + " " + param), "OrgaManager", JOptionPane.PLAIN_MESSAGE);
+		try {
+			String com = command + " " + param;
+			Process p = Runtime.getRuntime().exec(com);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line=reader.readLine();
+            while (line != null) {    
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch(IOException e1) { return false; 
+        } catch(InterruptedException e2) { return false;}
+        System.out.println("exec finished.");
+//		ProcessBuilder call = new ProcessBuilder(command, param);
+//	    try {
+//	        Process process = call.start();
+//	        process.waitFor();
+//	    } catch (IOException e) {
+//	        e.printStackTrace();
+//	        return false;
+//	    } catch (InterruptedException e) {
+//	        e.printStackTrace();
+//	        return false;
+//	    }
+	    return true;
+	}
+    
 	
 	public void setProperty(String key, String value){
 		Writer writer = null;
